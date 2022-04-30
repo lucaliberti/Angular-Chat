fc1c8ce5d6149b6e6303a46fe25fa4676962dc59
lucaliberti <liberti.luca@gmail.com>
2022-04-30 10:24:11 +0200

    first commit

---
+++ Chat-Angular-Client/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { ChatService } from './chat.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+
+
+export class AppComponent {
+  newMessage: string="";
+  messageList: string[] = [];
+
+  constructor(private chatService: ChatService){
+
+  }
+
+  ngOnInit(){
+    this.chatService.getNewMessage().subscribe((message: string) => {
+      this.messageList.push(message);
+    })
+  }
+
+  sendMessage() {
+    this.chatService.sendMessage(this.newMessage);
+    this.newMessage = '';
+  }
+}
+
+
+
+
